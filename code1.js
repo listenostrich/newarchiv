ff1486c3da72deb986cef8210fb5142a24b1afbe
Chuck Cecil <163863149+listenostrich@users.noreply.github.com>
2024-03-18 15:15:41 +0200

    Create code1.js

---
+++ code1.js
@@ -0,0 +1,26 @@
+ const rootNode = this.findNode(root);
+    recursivePosition(rootNode, 0, 0);
+  }
+
+  layoutRandom() {
+    this.callLayout = { method: this.layoutRandom, args: arguments };
+    const rect = this.getRect();
+    const placedNodes = [];
+    for (const node of this.nodes) {
+      do {
+        node.x = rect.left + Math.random() * rect.width;
+        node.y = rect.top + Math.random() * rect.height;
+      } while (placedNodes.find(placedNode => distance(node, placedNode) < 48));
+      placedNodes.push(node);
+    }
+  }
+
+   const edge = this.findEdge(source, target);
+    if (edge) edge.visitedCount += visit ? 1 : -1;
+    const node = this.findNode(target);
+    if (weight !== undefined) node.weight = weight;
+    node.visitedCount += visit ? 1 : -1;
+    if (this.logTracer) {
+      this.logTracer.println(visit ? (source || '') + ' -> ' + target : (source || '') + ' <- ' + target);
+    }
+  }
